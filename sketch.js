0fb53bf039bee54f542c5e8e868290b85ab0f6bd
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-04-09 19:36:25 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var stone, rubber,hammer;
+var stone, rubber,hammer,iron, plane;
 
 
 function setup(){
@@ -11,12 +11,8 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    plane = new Plane(600,height,1200,20)
-    iron = new Iron(300,350);
-    stone = new Stone(700,320,100,100);
-  
-    rubber=new Rubber(900,450,70);
-    hammer = new Hammer(10,100);
+    //create the objects
+    
 
   
 
